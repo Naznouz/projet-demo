86a669e015a0a1d65ef267d8e6c2ce77002f5e9e
TopNiz <nizar.ayed@upgrade-code.org>
2023-03-14 10:32:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Hello world!');
+
+let nombres = [1, 13, 25, 3, 15, 11, 50, 23];
+
+function sum(liste) {
+  return liste.reduce((acc, value) => acc + value, 0);
+}
+
+console.log('Somme: ', sum(nombres));
